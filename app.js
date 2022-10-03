37094df9b75893710614d41f93a25c220c493e13
Houda Elbaidak <houdaelbaidak6@gmail.com>
2022-10-03 23:36:12 +0100

    done

---
+++ app.js
@@ -0,0 +1,49 @@
+let form = document.querySelector("form");
+let output = document.getElementById("output");
+let inputValue = document.querySelector("#input");
+
+async function getUser(url) {
+  try {
+    let userdata = await fetch(url);
+    return userdata.json();
+    cardProfile(res);
+  } catch (error) {
+    output.textContent = "Erreur de connexion ";
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getUser("https://api.github.com/users/" + inputValue.value).then(
+    (profile) => {
+      console.log(profile);
+
+      createCard(profile);
+    }
+  );
+});
+
+function createCard(user) {
+  let container = document.querySelector(".cardContainer");
+  container.classList.add("cardContainer")
+  container.innerHTML = `
+  <div id="profileCard">
+      <div class="imgDiv">
+        <img
+          src="${user.avatar_url}"
+          alt="imgProfile"
+        />
+      </div>
+      <div class="contentDiv">
+        <h3>${user.name}</h3>
+        <p class="bio">
+        ${user.bio}
+        </p>
+        <ul>
+          <li><span>${user.followers}</span> &nbsp; Followers</li>
+          <li><span>${user.following}</span> &nbsp; Following</li>
+          <li><span>${user.public_repos}</span> &nbsp; Public Repos</li>
+        </ul>
+      </div>
+    </div>`;
+}
